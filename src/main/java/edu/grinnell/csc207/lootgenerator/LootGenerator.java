d9665a7f9eb1216131c7b22d1b3b324900c4491c
suzuneyagi <suzuneyagi@Suzune-MacBook-Air.local>
2025-11-14 13:13:39 -0600

    Fininsh implementing methods. Need to prompt user to generate additional item

--- src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
+++ src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
@@ -214,23 +214,111 @@ public class LootGenerator {
         int defenseValue = rnd.nextInt(stats.maxac - stats.minac + 1) + stats.minac;
         return defenseValue;
     }
+
+    public static class Affix {
+        String name;
+        String mod1code;
+        int mod1min;
+        int mod1max;
+
+        public Affix(String name, String mod1code, int mod1min, int mod1max) {
+            this.name = name;
+            this.mod1code = mod1code;
+            this.mod1min = mod1min;
+            this.mod1max = mod1max;
+        }
+    }
+
+    public static ArrayList<Affix> affixParse(String filename) throws IOException {
+        ArrayList<Affix> affixes = new ArrayList<>();
+        Scanner text = new Scanner(new File(filename));
+
+        while (text.hasNextLine()) {
+            String line = text.nextLine();
+            String[] parts = line.split("\t");
+            String name = parts[0];
+            String mod1code = parts[1];
+            int mod1min = Integer.parseInt(parts[2]);
+            int mod1max = Integer.parseInt(parts[3]);
+            Affix affix = new Affix(name, mod1code, mod1min, mod1max);
+            affixes.add(affix);
+        }
+
+        text.close();
+        return affixes;
+    }
+
+    public static void generateAffix(String baseItem) throws IOException{
+        String prefixName = "";
+        String suffixName = "";
+        String prefixStats = "";
+        String suffixStats = "";
+        int prefixValue = 0;
+        int suffixValue = 0;
+
+        ArrayList<Affix> prefix = affixParse(DATA_SET + "/MagicPrefix.txt");
+        ArrayList<Affix> suffix = affixParse(DATA_SET + "/MagicSuffix.txt");
+        
+        Random prefixBoolean = new Random();
+        boolean isPrefix = prefixBoolean.nextBoolean();
+
+        Random suffixBoolean = new Random();
+        boolean isSuffix = suffixBoolean.nextBoolean();
+
+        if (isPrefix) {
+            Random prefixInt = new Random();
+            int prefixIndex = prefixInt.nextInt(prefix.size());
+            Affix selectedPrefix = prefix.get(prefixIndex);
+            prefixName = selectedPrefix.name + " ";
+            prefixStats = selectedPrefix.mod1code;
+
+            Random preStats = new Random();
+            prefixValue = preStats.nextInt(selectedPrefix.mod1max - selectedPrefix.mod1min + 1) + selectedPrefix.mod1min;
+        }
+
+        if (isSuffix) {
+            Random suffixInt = new Random();
+            int suffixIndex = suffixInt.nextInt(suffix.size());
+            Affix selectedSuffix = suffix.get(suffixIndex);
+            suffixName = " " + selectedSuffix.name;
+            suffixStats = selectedSuffix.mod1code;
+
+            Random sufStats = new Random();
+            suffixValue = sufStats.nextInt(selectedSuffix.mod1max - selectedSuffix.mod1min + 1) + selectedSuffix.mod1min;
+        }
+
+        String defenseValue = Integer.toString(generateBaseStats(baseItem));
+
+        System.out.println(prefixName + baseItem + suffixName);
+        System.out.println("Defense: " + defenseValue);
+
+        if (isPrefix) {
+            System.out.println(prefixValue + " " + prefixStats);
+        }
+
+        if (isSuffix) {
+            System.out.println(suffixValue + " " + suffixStats);
+        }
+
+    }
     
+
     public static void main(String[] args) throws IOException{
-        System.out.println("This program kills monsters and generates loot!");
-        // TOOD: Implement me!
         String monsterName = pickMonster();
         String treasureClass = fetchTreasureClass(monsterName);
         String baseItem = generateBaseItem(treasureClass);
-        String defenseValue = Integer.toString(generateBaseStats(baseItem));
-        String prefix = "Prefix ";
-        String suffix = " Suffix";
+        // String defenseValue = Integer.toString(generateBaseStats(baseItem));
+        // String prefix = "Prefix ";
+        // String suffix = " Suffix";
 
+        System.out.println("This program kills monsters and generates loot!");
         System.out.println("Fighting " + monsterName);
         System.out.println("You have slain " + monsterName + "!");
         System.out.println(monsterName + " dropped: ");
         System.out.println();
-        System.out.println(prefix + baseItem + suffix);
-        System.out.println("Defense: " + defenseValue);
+        generateAffix(baseItem);
+        // System.out.println(prefix + baseItem + suffix);
+        //System.out.println("Defense: " + defenseValue);
     }
     }
 
